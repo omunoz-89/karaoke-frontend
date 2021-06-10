9c90d813337a6f9e7610fd2fa3bc2045cf190d96
davemolk <davemolk@gmail.com>
2021-06-10 08:14:27 -0600

    VideoPlayer component to integrate into Video.js

---
+++ src/components/VideoPlayer.js
@@ -0,0 +1,17 @@
+import ReactPlayer from "react-player";
+
+const VideoPlayer = () => {
+  return (
+    <div>
+      <ReactPlayer
+        controls
+        url="https://www.youtube.com/watch?v=C3y6jGCXiUA"
+        onStart={() => console.log("onStart callback")}
+        onEnded={() => console.log("onEnded callback")}
+        onError={() => console.log("onError callback")}
+      />
+    </div>
+  );
+};
+
+export default VideoPlayer;
