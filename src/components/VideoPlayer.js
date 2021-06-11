9c1728136dc092a5834c829c129cb6d07a591b75
davemolk <davemolk@gmail.com>
2021-06-11 13:03:34 -0600

    update youtube search, videoplayer now on record page

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -1,11 +1,11 @@
 import ReactPlayer from "react-player/youtube";
 
-const VideoPlayer = () => {
+const VideoPlayer = (props) => {
   return (
     <div>
       <ReactPlayer
         controls
-        url="https://www.youtube.com/watch?v=C3y6jGCXiUA"
+        url={`https://www.youtube.com/watch?v=${props.ytURL}`}
         onStart={() => console.log("onStart callback")}
         onEnded={() => console.log("onEnded callback")}
         onError={() => console.log("onError callback")}
