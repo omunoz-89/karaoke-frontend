bf6eaeb7e4de3b51f47d4e194e1ed5c8c1d81e65
davemolk <davemolk@gmail.com>
2021-06-13 22:04:13 -0600

    set YT results to 15, use card layout for results

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,25 +1,39 @@
-import React, {useEffect, useState} from 'react'
-import {Link} from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const Results = (props) => {
-    
-    console.log(props)
-    let results = props.results.data.items
-    const resultsList = results.map((r, idx) => {
-       return <div>
-        <Link to= {`/record/${r.id.videoId}`} params={{ video: r.id.videoId }}>
-           <img src={r.snippet.thumbnails.medium.url} alt={r.snippet.title} />
-       <h5 key={idx}>{r.snippet.title}</h5>
-       </Link>
-       <hr />
-       </div>
-    })
-
-    return(
-        <div>
-            {resultsList}
+  console.log(props);
+  let results = props.results.data.items;
+  const resultsList = results.map((r, idx) => {
+    return (
+      <div className="col">
+        <div key={idx} class="card">
+          <img
+            className="cardImg"
+            src={r.snippet.thumbnails.medium.url}
+            alt={r.snippet.title}
+          />
+          <div className="card-body">
+            <h5 className="card-title">{r.snippet.title}</h5>
+            <Link
+              to={`/record/${r.id.videoId}`}
+              params={{ video: r.id.videoId }}
+            ></Link>
+          </div>
         </div>
-    )
-}
+        <hr />
+      </div>
+    );
+  });
 
-export default Results
+  return (
+    <div>
+      <h1>Search Results</h1>
+      <div className="container">
+        <div className="row">{resultsList}</div>;
+      </div>
+    </div>
+  );
+};
+
+export default Results;
