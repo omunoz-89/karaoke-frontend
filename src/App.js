b215feff43121b00ba9095dfbd7099ec6ca2c336
anthonymang <anthonymanganielloda@gmail.com>
2021-06-14 09:21:29 -0400

    .

--- src/App.js
+++ src/App.js
@@ -13,30 +13,34 @@ import Profile from "./components/Profile";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import About from "./components/About";
-import User from "./components/User"
-import Video from "./components/Video"
+import User from "./components/User";
+import Video from "./components/Video";
 import Results from "./components/Results";
 import Record from "./components/Record";
-
-const CONNECTION_URI = process.env.REACT_APP_SERVER_URL
-const KEY = process.env.API_KEY
-
-
+import Trending from "./components/Trending";
+const CONNECTION_URI = process.env.REACT_APP_SERVER_URL;
+const KEY = process.env.API_KEY;
 //Private route component
-const PrivateRoute = ({component: Component, ...rest}) => {
-  console.log('This is a private route')
-  let user = localStorage.getItem('jwtToken')
-  return <Route {...rest} render={ (props) => {
-    return user ? <Component {...rest} {...props} /> : <Redirect to='/login' />
-  }} />
-}
-
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  console.log("This is a private route");
+  let user = localStorage.getItem("jwtToken");
+  return (
+    <Route
+      {...rest}
+      render={(props) => {
+        return user ? (
+          <Component {...rest} {...props} />
+        ) : (
+          <Redirect to="/login" />
+        );
+      }}
+    />
+  );
+};
 function App() {
   // Set state values
   const [currentUser, setCurrentUser] = useState("");
   const [isAuthenticated, setIsAuthenticated] = useState(true);
-
-
   useEffect(() => {
     let token;
     //if there is token inside of local storage, then the user is not authenticated
@@ -48,35 +52,36 @@ function App() {
       console.log("token", token);
       setAuthToken(token);
       setCurrentUser(token);
-    }}, [])
-
-
+    }
+  },[]);
   const nowCurrentUser = (userData) => {
     console.log("--- inside nowCurrentUser ---");
     setCurrentUser(userData);
     setIsAuthenticated(true);
   };
   const handleLogout = () => {
-    if(localStorage.getItem("jwtToken")) {    //determine if there is a jwt token
-      localStorage.removeItem('jwtToken')    //remove if thre is a jwt
-      setCurrentUser(null);     //set currentUser to null
-      setIsAuthenticated(false)     //set auth to false
+    if (localStorage.getItem("jwtToken")) {
+      //determine if there is a jwt token
+      localStorage.removeItem("jwtToken"); //remove if thre is a jwt
+      setCurrentUser(null); //set currentUser to null
+      setIsAuthenticated(false); //set auth to false
     }
   };
-
   return (
     <div className="App">
       <Navbar isAuth={isAuthenticated} handleLogout={handleLogout} />
       <div className="container5">
         <Switch>
-            {/* PUBLIC ROUTES */}
-          <Route path='/about' component={About} />
-          <Route path='/record/:video' render={(...routeProps) => {
-              return <Record {...routeProps} />
-          }} />
-
-          <Route exact path='/' component={Welcome}/>
-          <Route exact path='/search/results' component={Results}/>
+          {/* PUBLIC ROUTES */}
+          <Route path="/about" component={About} />
+          <Route
+            path="/record/:video"
+            render={(...routeProps) => {
+              return <Record {...routeProps} />;
+            }}
+          />
+          <Route exact path="/" component={Welcome} />
+          <Route exact path="/search/results" component={Results} />
           {/* PUBLIC ROUTES */}
           <Route
             path="/signup"
@@ -95,21 +100,24 @@ function App() {
               />
             )}
           />
-
           <Route
             path="/users/:id"
             render={(routeProps) => {
               return <User {...routeProps} />;
             }}
           />
-
+          <Route
+            exact path="/videos/trending"
+            render={(routeProps) => {
+              return <Trending {...routeProps} />;
+            }}
+          />
           <Route
             path="/videos/:id"
             render={(routeProps) => {
               return <Video {...routeProps} />;
             }}
           />
-
           <PrivateRoute
             path="/profile"
             component={Profile}
@@ -122,5 +130,4 @@ function App() {
     </div>
   )
 }
-
 export default App;
