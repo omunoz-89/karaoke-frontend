acc835bf21cb2405a564f28eaebc263f69177f92
Eddyfu45 <eddyfu45@yahoo.com>
2021-06-14 11:31:37 -0400

    Welcome Page/Search Page Updated

--- src/components/Results.js
+++ src/components/Results.js
@@ -30,7 +30,7 @@ const Results = (props) => {
 
   return (
     <div>
-      <h1>Search Results</h1>
+      <h1 className="searchResultsText">Search Results</h1>
       <div className="container">
         <div className="row">{resultsList}</div>;
       </div>
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,46 +3,36 @@ import Search from "./Search";
 import VideoPlayer from "./VideoPlayer";
 
 const Welcome = () => {
-<<<<<<< HEAD
-    return (
-        <div>
-            <h1 className="welcomeText">Welcome</h1>
-            <img className="mic" src="https://res.cloudinary.com/sei412-om/image/upload/v1623440005/mic_pmdu7o.png"/>
-            {/* <h1>Welcome</h1> */}
-            <Search />
-        </div>
-    )
-}
-=======
   // const ytURL = "ubTIBlx4xTw?t=23";
   return (
     <div>
       <h1 className="welcomeText">
         Welcome to Cheraoke, you beautiful person you!
       </h1>
-      <img
-        className="mic"
-        src="https://i.pinimg.com/474x/61/09/36/61093605aca4cf910bc4eb65eaa8141f.jpg"
-      />
-      <p>
+      <p className="introText">
         Enter a song and artist, click submit, and get ready for glory! Need
         some inspiration? Unmute the video and bask in the musical muse. We
         believe in you!
       </p>
-      <iframe
-        width="100%"
-        height="480"
-        src="https://www.youtube.com/embed/ubTIBlx4xTw"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowFullScreen
-        title="Embedded video"
-      />
-      <Search />
+      {/* <img
+        className="mic"
+        src="https://res.cloudinary.com/sei412-om/image/upload/v1623440005/mic_pmdu7o.png"
+      /> */}
+      <div className="welcomeVid">
+        <iframe
+          width="40%"
+          height="350"
+          src="https://www.youtube.com/embed/ubTIBlx4xTw"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+          title="Embedded video"
+        />
+        <Search />
       {/* <VideoPlayer ytURL={ytURL} /> */}
+      </div>
     </div>
   );
 };
->>>>>>> 3dc8228a440463bff694e3a9417e1274f0abcc9a
 
 export default Welcome;
